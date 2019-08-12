93d615f13487402b3db40006dc3e57037176b632
Nadav Oran <nadavoran@gmail.com>
2019-08-13 01:17:32 +1000

    second try

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const { Player } = require('./Player');
 const { Hand } = require('./Hand');
 const CardData = require('./Card');
 
+export { default as Board } from './Board';
+export { default as Player } from './Player';
+export { default as Hand } from './Hand';
+export { default as CardData } from './Card';
+
+
 
 module.exports = {
     Board, Player, Hand, CardData
